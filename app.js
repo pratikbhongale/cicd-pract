b80f474ef98d51c6007f39019de4f51d1c06b49e
pratikbhongale <pratikbhongale01@gmail.com>
2026-03-13 10:31:05 +0530

    Code

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+app.get("/", (req,res) => {
+    res.send("CI/CD pipeline working!");
+});
+app.listen(3000, () => {
+    console.log("Server running on port");
+});
